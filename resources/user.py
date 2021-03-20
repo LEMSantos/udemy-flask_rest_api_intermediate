77b2a08b7a393ba9b4c37afd4eaf9faf25f07115
LEMSantos <lucas_m-santos@hotmail.com>
2021-03-20 15:34:21 -0300

    Md - reource do usuário

--- resources/user.py
+++ resources/user.py
@@ -1,24 +1,32 @@
 from flask_restful import Resource, reqparse
 from http import HTTPStatus
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity,
+    get_jwt,
+)
 from models.user import UserModel
+from blocklist import BLOCKLIST
 
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument(
+    'username',
+    required=True,
+    help='This field cannot be blank.'
+)
+_user_parser.add_argument(
+    'password',
+    required=True,
+    help='This field cannot be blank.'
+)
 
 class UserRegister(Resource):
 
-    parser = reqparse.RequestParser()
-    parser.add_argument(
-        'username',
-        required=True,
-        help='This field cannot be blank.'
-    )
-    parser.add_argument(
-        'password',
-        required=True,
-        help='This field cannot be blank.'
-    )
-
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
 
         if UserModel.find_by_username(data.get('username')):
             return {
@@ -31,3 +39,69 @@ class UserRegister(Resource):
         return {
             'message': 'User created successfully.',
         }, HTTPStatus.CREATED
+
+
+class User(Resource):
+
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+
+        if not user:
+            return {'message': 'User not found'}, HTTPStatus.NOT_FOUND
+
+        return {'data': user.json()}
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+
+        if not user:
+            return {'message': 'User not found'}, HTTPStatus.NOT_FOUND
+
+        user.delete()
+
+        return {'message': 'User deleted.'},
+
+
+class UserLogin(Resource):
+
+    def post(self):
+        data = _user_parser.parse_args()
+        username = data.get('username')
+        password = data.get('password')
+
+        user = UserModel.find_by_username(username)
+
+        if user and safe_str_cmp(user.password, password):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+
+            return {
+                'access_token': access_token,
+                'refresh_token': refresh_token,
+            }
+
+        return {'message': 'Invalid credentials'}, HTTPStatus.UNAUTHORIZED
+
+
+class UserLogout(Resource):
+
+    @jwt_required()
+    def post(self):
+        jti = get_jwt().get('jti')
+        BLOCKLIST.add(jti)
+
+        return {'message': 'Successfully logged out.'}
+
+
+class TokenRefresh(Resource):
+
+    @jwt_required(refresh=True)
+    def post(self):
+        user_id = get_jwt_identity()
+        access_token = create_access_token(identity=user_id, fresh=False)
+
+        return {
+            'access_token': access_token,
+        }
